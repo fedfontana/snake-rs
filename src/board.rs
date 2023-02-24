deb9dce356f3c5cf38153718f7599ab90cab2021
fedfontana <federico.fontana2000@gmail.com>
2023-02-24 18:12:36 +0100

    added functionality, still need to test it

--- src/board.rs
+++ src/board.rs
@@ -1,10 +1,10 @@
 use std::fmt::Display;
 
-use crate::snake::{Position, Snake};
+use crate::snake::{Direction, Position, Snake};
 
 pub struct Board {
     width: usize,
-    height: usize, 
+    height: usize,
     fruits: Vec<Position>,
     snake: Snake,
 }
@@ -15,35 +15,84 @@ impl Board {
             width: w,
             height: h,
             fruits: Vec::new(),
-            snake: Snake::new(w, h)
+            snake: Snake::new(w, h),
         }
+    }
+    /// Advances in the direction of the head. Returns whether the snake hit a wall or itself while advancing.
+    pub fn advance_snake(&mut self) -> bool {
+        let Some(head) = self.snake.pieces.front().cloned() else { return true };
+
+        self.snake.pieces.pop_back();
+
+        let new_position = match self.snake.facing {
+            Direction::Up => {
+                if head.row > 0 {
+                    Some(Position::new(head.row - 1, head.col))
+                } else {
+                    None
+                }
+            }
+            Direction::Down => {
+                if head.row + 1 <= self.height {
+                    Some(Position::new(head.row + 1, head.col))
+                } else {
+                    None
+                }
+            }
+            Direction::Left => {
+                if head.col > 0 {
+                    Some(Position::new(head.row, head.col - 1))
+                } else {
+                    None
+                }
+            }
+            Direction::Right => {
+                if head.col + 1 <= self.width {
+                    Some(Position::new(head.row, head.col + 1))
+                } else {
+                    None
+                }
+            }
+        };
+
+        // if the new position is not out of bounds assin it to new_position, else return false
+        let Some(new_position) = new_position else { return false };
+
+        // return true if the snake hit itself
+        if self.snake.pieces.contains(&new_position) {
+            return true;
+        }
+
+        self.snake.pieces.push_front(new_position);
+        
+        false
     }
 }
 
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut repr = vec!["_"; (self.width+1) * self.height];
+        let mut repr = vec!["_"; (self.width + 1) * self.height];
         for row in 0..self.height {
-            repr[row * (self.width+1)] = "\n";
+            repr[row * (self.width + 1)] = "\n";
         }
-        
+
         for fruit in self.fruits.iter() {
-            repr[fruit.row * (self.width+1) + fruit.col] = "x";
+            repr[fruit.row * (self.width + 1) + fruit.col] = "x";
         }
 
         for (idx, part) in self.snake.pieces.iter().enumerate() {
             let cell_repr = if idx == 0 {
                 match self.snake.facing {
-                    crate::snake::Direction::Up => "^",
-                    crate::snake::Direction::Down => "V",
-                    crate::snake::Direction::Left => "<",
-                    crate::snake::Direction::Right => ">",
+                    Direction::Up => "^",
+                    Direction::Down => "V",
+                    Direction::Left => "<",
+                    Direction::Right => ">",
                 }
             } else {
                 "#"
             };
 
-            repr[part.row * (self.width+1) + part.col + 1] = cell_repr;            
+            repr[part.row * (self.width + 1) + part.col + 1] = cell_repr;
         }
 
         write!(f, "{}", repr.join(""))
--- src/snake.rs
+++ src/snake.rs
@@ -1,3 +1,6 @@
+use std::collections::VecDeque;
+
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub struct Position {
     pub row: usize,
     pub col: usize,
@@ -15,18 +18,27 @@ pub enum Direction {
 }
 
 pub struct Snake {
+    /// The direction faced by the head of the snake
     pub facing: Direction,
-    pub pieces: Vec<Position>,
+    /// The positions of the segments of the snake. Segments are ordered, with the head in position 0.
+    pub pieces: VecDeque<Position>,
 }
 
 impl Snake {
     pub fn new(canvas_w: usize, canvas_h: usize) -> Self {
+        let mut pieces = VecDeque::new();
+        pieces.push_back(Position::new(canvas_h / 2, canvas_w / 2));
         Self {
             facing: Direction::Right,
-            pieces: vec![Position {
-                col: canvas_w / 2,
-                row: canvas_h / 2,
-            }],
+            pieces,
         }
+    }
+
+    pub fn set_direction(&mut self, dir: Direction) {
+        self.facing = dir;
+    }
+
+    pub fn grow(&mut self, new_segment_pos: Position) {
+        self.pieces.push_back(new_segment_pos);
     }
 }
